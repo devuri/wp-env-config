70043f14171adfa8be9894e155ad7554af0098a3
devuri <devuriko@gmail.com>
2021-07-16 07:58:37 -0500

    Adds examples for wp-config.php and composer.json in example dir

---
+++ example/wp-config.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once  dirname( __FILE__ ) . '/vendor/autoload.php';
+
+use DevUri\Config\Setup;
+
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+Setup::init(__DIR__)->config('secure'); // development | staging | production | secure
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
