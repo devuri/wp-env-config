47d33b852bdf0caf8d1e40a5e8ac960b92449f49
devuri <devuriko@gmail.com>
2022-08-14 01:37:25 -0500

    feat: Adds `asset_url()`

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -35,4 +35,15 @@ class HelpersTest extends BaseTest
 
         $this->assertSame($static_path, $asset_url);
     }
+
+    public function test_asset_url_return_url_only(): void
+    {
+        $asset_url = asset_url() . "images/thing.png";
+
+        $this->assertIsString($asset_url);
+
+        $url = "https://example.com/assets/dist/images/thing.png";
+
+        $this->assertSame($url, $asset_url);
+    }
 }
