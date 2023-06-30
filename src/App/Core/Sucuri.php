ca71cc7e7ae068c0acf312f9b66b3af9407dd7ea
devuri <devuri@users.noreply.github.com>
2023-06-29 22:04:33 -0500

    chore: build

--- src/App/Core/Sucuri.php
+++ src/App/Core/Sucuri.php
@@ -20,16 +20,16 @@ class Sucuri
      */
     public function __construct()
     {
-		add_action( 'plugins_loaded', [$this, 'init_action' ] );
+        add_action( 'plugins_loaded', [ $this, 'init_action' ] );
     }
 
-	public function  init_action(): void
-	{
-		if ( current_user_can( 'manage_options' ) ) {
+    public function init_action(): void
+    {
+        if ( current_user_can( 'manage_options' ) ) {
             add_action( 'init', [ $this, 'remove_sucuri_admin_ui' ] );
             add_action( 'init', [ $this, 'disable_firewall_ui' ] );
         }
-	}
+    }
 
     /**
      * Disable Sucuri WAF admin stuff.
@@ -41,18 +41,21 @@ class Sucuri
         }
 
         add_action( 'admin_menu', [ $this, 'remove_firewall_submenu' ] );
-		add_action( 'admin_enqueue_scripts', function ( $hook ): void
-		    {
-				if ( 'DEBUG' === strtoupper( HTTP_ENV_CONFIG ) || 'DEB' === strtoupper( HTTP_ENV_CONFIG ) ) {
-					return;
-				}
+        add_action(
+            'admin_enqueue_scripts',
+            function ( $hook ): void {
+                if ( 'DEBUG' === strtoupper( HTTP_ENV_CONFIG ) || 'DEB' === strtoupper( HTTP_ENV_CONFIG ) ) {
+                    return;
+                }
 
-		        $style = '.sucuriscan-hstatus-0 {display:none !important;}';
-		        wp_add_inline_style( 'sucuriscan', $style );
-		}, 99 );
+                $style = '.sucuriscan-hstatus-0 {display:none !important;}';
+                wp_add_inline_style( 'sucuriscan', $style );
+            },
+            99
+        );
     }
 
-	/**
+    /**
      * Removes Sucuri admin UI if user is not sudo admin.
      */
     public function remove_sucuri_admin_ui(): void
@@ -60,10 +63,10 @@ class Sucuri
         if ( \defined( 'WP_SUDO_ADMIN' ) && is_user_logged_in() ) {
             $current_user = wp_get_current_user();
 
-			$sucuri_menu_prefix = is_multisite() ? 'network_' : '';
+            $sucuri_menu_prefix = is_multisite() ? 'network_' : '';
 
             if ( WP_SUDO_ADMIN !== $current_user->ID ) {
-				remove_action( 'admin_menu', $sucuri_menu_prefix . 'sucuriscanAddMenuPage' );
+                remove_action( 'admin_menu', $sucuri_menu_prefix . 'sucuriscanAddMenuPage' );
             }
         }
     }
@@ -73,8 +76,8 @@ class Sucuri
      */
     public function remove_firewall_submenu(): void
     {
-		global $submenu;
+        global $submenu;
 
-		unset( $submenu['sucuriscan'][1] );
+        unset( $submenu['sucuriscan'][1] );
     }
 }
