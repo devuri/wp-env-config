80924a496de29b8b8c6389ec6dca91930b996a6b
devuri <devuri@users.noreply.github.com>
2023-07-01 14:18:37 -0500

    feat: use `ConstantTrait` and add `redis` and `security` settings to BaseKernel

--- src/App/Http/BaseKernel.php
+++ src/App/Http/BaseKernel.php
@@ -2,7 +2,6 @@
 
 namespace Urisoft\App\Http;
 
-use function define;
 use function defined;
 
 use Exception;
@@ -16,6 +15,8 @@ use Urisoft\App\Setup;
  */
 class BaseKernel
 {
+    use ConstantTrait;
+
     protected $app_path;
     protected $log_file;
     protected $dir_name;
@@ -41,6 +42,8 @@ class BaseKernel
         'config_file'     => 'config',
         'sudo_admin'      => null,
         'sucuri_waf'      => false,
+        'redis'           => [],
+        'security'        => [],
     ];
 
     /**
@@ -51,7 +54,7 @@ class BaseKernel
      *
      * @throws Exception
      */
-    public function __construct( string $app_path, array $args = [] )
+    public function __construct( string $app_path, array $args = [], ?Setup $setup = null )
     {
         $this->app_path = $app_path;
 
@@ -95,12 +98,21 @@ class BaseKernel
          *
          * @link https://github.com/vlucas/phpdotenv/pull/394
          */
-        $this->app_setup = Setup::init( $this->app_path );
+        if ( \is_null( $setup ) ) {
+            $this->app_setup = Setup::init( $this->app_path );
+        } else {
+            $this->app_setup = $setup;
+        }
     }
 
     public function get_app(): Setup
     {
         return $this->app_setup;
+    }
+
+    public function get_app_security(): array
+    {
+        return $this->args['security'];
     }
 
     public function get_app_path(): string
@@ -179,97 +191,6 @@ class BaseKernel
         if ( true === $constants ) {
             $this->constants();
         }
-    }
-
-    /**
-     * Defines constants.
-     *
-     * @psalm-suppress UndefinedConstant
-     *
-     * @return void
-     */
-    public function constants(): void
-    {
-        // define app_path.
-        $this->define( 'APP_PATH', $this->get_app_path() );
-
-        // define public web root dir.
-        $this->define( 'PUBLIC_WEB_DIR', APP_PATH . '/' . $this->args['web_root'] );
-
-        // wp dir path
-        $this->define( 'WP_DIR_PATH', PUBLIC_WEB_DIR . '/' . $this->args['wp_dir_path'] );
-
-        // define assets dir.
-        $this->define( 'APP_ASSETS_DIR', PUBLIC_WEB_DIR . '/' . $this->args['asset_dir'] );
-
-        // Directory PATH.
-        $this->define( 'APP_CONTENT_DIR', '/' . $this->args['content_dir'] );
-        $this->define( 'WP_CONTENT_DIR', PUBLIC_WEB_DIR . APP_CONTENT_DIR );
-
-        // Content Directory.
-        $this->define( 'CONTENT_DIR', APP_CONTENT_DIR );
-        $this->define( 'WP_CONTENT_URL', env( 'WP_HOME' ) . CONTENT_DIR );
-
-        /*
-         * Themes, prefer '/templates'
-         *
-         * This requires mu-plugin or add `register_theme_directory( APP_THEME_DIR );`
-         *
-         * @link https://github.com/devuri/custom-wordpress-theme-dir
-         */
-        if ( $this->args['templates_dir'] ) {
-            $this->define( 'APP_THEME_DIR', PUBLIC_WEB_DIR . '/' . $this->args['templates_dir'] );
-        }
-
-        // Plugins.
-        $this->define( 'WP_PLUGIN_DIR', PUBLIC_WEB_DIR . '/' . $this->args['plugin_dir'] );
-        $this->define( 'WP_PLUGIN_URL', env( 'WP_HOME' ) . '/' . $this->args['plugin_dir'] );
-
-        // Must-Use Plugins.
-        $this->define( 'WPMU_PLUGIN_DIR', PUBLIC_WEB_DIR . '/' . $this->args['mu_plugin_dir'] );
-        $this->define( 'WPMU_PLUGIN_URL', env( 'WP_HOME' ) . '/' . $this->args['mu_plugin_dir'] );
-
-        // Disable any kind of automatic upgrade.
-        // this will be handled via composer.
-        $this->define( 'AUTOMATIC_UPDATER_DISABLED', $this->args['disable_updates'] );
-
-        // Sudo admin (granted more privilages uses user ID).
-        $this->define( 'WP_SUDO_ADMIN', $this->args['sudo_admin'] );
-
-        /*
-         * Prevent Admin users from deactivating plugins, true or false.
-         *
-         * @link https://gist.github.com/devuri/034ccb7c833f970192bb64317814da3b
-         */
-        $this->define( 'CAN_DEACTIVATE_PLUGINS', $this->args['can_deactivate'] );
-
-        // SQLite database location and filename.
-        $this->define( 'DB_DIR', APP_PATH . '/' . $this->args['sqlite_dir'] );
-        $this->define( 'DB_FILE', $this->args['sqlite_file'] );
-
-        /*
-         * Slug of the default theme for this installation.
-         * Used as the default theme when installing new sites.
-         * It will be used as the fallback if the active theme doesn't exist.
-         *
-         * @see WP_Theme::get_core_default_theme()
-         */
-        $this->define( 'WP_DEFAULT_THEME', $this->args['default_theme'] );
-
-        // home url md5 value.
-        $this->define( 'COOKIEHASH', md5( env( 'WP_HOME' ) ) );
-
-        // Defines cookie-related override for WordPress constants.
-        $this->define( 'USER_COOKIE', 'wpc_user_' . COOKIEHASH );
-        $this->define( 'PASS_COOKIE', 'wpc_pass_' . COOKIEHASH );
-        $this->define( 'AUTH_COOKIE', 'wpc_' . COOKIEHASH );
-        $this->define( 'SECURE_AUTH_COOKIE', 'wpc_sec_' . COOKIEHASH );
-        $this->define( 'LOGGED_IN_COOKIE', 'wpc_logged_in_' . COOKIEHASH );
-        $this->define( 'TEST_COOKIE', md5( 'wpc_test_cookie' . env( 'WP_HOME' ) ) );
-
-        // SUCURI
-        $this->define( 'ENABLE_SUCURI_WAF', $this->args['sucuri_waf'] );
-        $this->define( 'SUCURI_DATA_STORAGE', ABSPATH . '../../storage/logs/sucuri' );
     }
 
     /**
