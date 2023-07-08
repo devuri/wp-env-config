1f4bc3ccab6a42f0bb6933ad4c2cd4d3d16b222b
devuri <devuri@users.noreply.github.com>
2023-07-08 18:27:47 -0500

    chore: code build

--- src/App/Console/S3Uploader.php
+++ src/App/Console/S3Uploader.php
@@ -10,56 +10,63 @@ class S3Uploader
     private $s3Client;
     private $bucketName;
 
-    public function __construct($accessKeyId, $secretAccessKey, $region, $bucketName)
+    public function __construct( $accessKeyId, $secretAccessKey, $region, $bucketName )
     {
         $this->bucketName = $bucketName;
-        $this->s3Client = new S3Client([
-            'version' => 'latest',
-            'region' => $region,
-            'credentials' => [
-                'key' => $accessKeyId,
-                'secret' => $secretAccessKey,
-            ],
-        ]);
+        $this->s3Client   = new S3Client(
+            [
+				'version'     => 'latest',
+				'region'      => $region,
+				'credentials' => [
+					'key'    => $accessKeyId,
+					'secret' => $secretAccessKey,
+				],
+			]
+        );
     }
 
-    public function createBucketIfNotExists(): string
+    public function createBucketIfNotExists(): ?bool
     {
         try {
-            $buckets = $this->s3Client->listBuckets();
+            $buckets      = $this->s3Client->listBuckets();
             $bucketExists = false;
 
-            foreach ($buckets['Buckets'] as $bucket) {
-                if ($bucket['Name'] === $this->bucketName) {
+            foreach ( $buckets['Buckets'] as $bucket ) {
+                if ( $bucket['Name'] === $this->bucketName ) {
                     $bucketExists = true;
 
                     break;
                 }
             }
 
-            if ( ! $bucketExists) {
-                $this->s3Client->createBucket(['Bucket' => $this->bucketName]);
+            if ( ! $bucketExists ) {
+                $this->s3Client->createBucket( [ 'Bucket' => $this->bucketName ] );
 
-                return "Bucket created successfully!";
+                return true;
             }
-        } catch (AwsException $e) {
-            return "Error creating bucket: " . $e->getMessage();
+        } catch ( AwsException $e ) {
+            return false;
         }
+
+		return null;
     }
 
-    public function uploadFile($localFilePath, $s3ObjectKey): bool
+    public function uploadFile( $localFilePath, $s3ObjectKey ): bool
     {
         try {
             $this->createBucketIfNotExists();
 
-            $this->s3Client->putObject([
-                'Bucket' => $this->bucketName,
-                'Key' => $s3ObjectKey,
-                'Body' => fopen($localFilePath, 'rb'), // Open file as a stream
-            ]);
+            $this->s3Client->putObject(
+                [
+					'Bucket' => $this->bucketName,
+					'Key'    => $s3ObjectKey,
+					'Body'   => fopen( $localFilePath, 'rb' ),
+                // Open file as a stream
+				]
+            );
 
             return true;
-        } catch (AwsException $e) {
+        } catch ( AwsException $e ) {
             return false;
         }
     }
