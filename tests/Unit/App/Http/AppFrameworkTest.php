defe80168d477f4420061bbf4aacf7e9f08ada8f
devuri <devuri@users.noreply.github.com>
2023-12-09 18:25:53 -0500

    refactor!: re `App.php` to `Urisoft\App\Http\AppFramework`

---
+++ tests/Unit/App/Http/AppFrameworkTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit\App\Http;
+
+use Tests\BaseTest;
+use Urisoft\App\Http\AppFramework;
+
+/**
+ * Test the Kernel.
+ *
+ * @internal
+ *
+ * @coversNothing
+ */
+class AppFrameworkTest extends BaseTest
+{
+    public function test_class_exists_is_true(): void
+    {
+        $this->assertTrue( class_exists('Urisoft\App\Http\AppFramework') );
+    }
+}
